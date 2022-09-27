b0495b0f6f91d1e8ecc5073fe3bd3653e7fa5e64
minbo2002 <minbo2002@naver.com>
2022-09-27 10:36:14 +0900

    Apply .gitignore

--- src/test/java/study/querydsl/SpringbootWebprojectQuerydslApplicationTests.java
+++ src/test/java/study/querydsl/SpringbootWebprojectQuerydslApplicationTests.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class SpringbootWebprojectQuerydslApplicationTests {
 
     @Autowired
